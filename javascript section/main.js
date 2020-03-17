3663d7fdd4b88b40fb579e3dfaae7ed074773a0e
mazaraki <dimitramaz@yahoo.gr>
2020-03-17 19:50:12 +0100

    2nd ccommit, commit CSS grid

---
+++ javascript section/main.js
@@ -0,0 +1,6 @@
+var a = document.getElementsByTagName('h1')
+a=document.getElementsByClassName('second')[0]
+// a = document.getElementById('first')
+// a = document.getElementsByTagName('p')
+// document.write(a)
+// console.log(a)
