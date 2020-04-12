30ee40323fbf5e88153b5d477ebf5ef9d3700f5c
mazaraki <dimitramaz@yahoo.gr>
2020-04-13 00:12:04 +0200

    Updates

---
+++ precache-manifest.ba89d4a0b8b5c5f63e3c1d1fb65b9e36.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "289a35cc1cee3b285502bba82b20330c",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "8cf97992ab0d8a7430e1",
+    "url": "/RoboFriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "b88560de760176d6aff7",
+    "url": "/RoboFriends/static/css/main.730464c2.chunk.css"
+  },
+  {
+    "revision": "8cf97992ab0d8a7430e1",
+    "url": "/RoboFriends/static/js/2.84d6e4e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.84d6e4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b88560de760176d6aff7",
+    "url": "/RoboFriends/static/js/main.3a83593a.chunk.js"
+  },
+  {
+    "revision": "9fda7ca17e43078694ea",
+    "url": "/RoboFriends/static/js/runtime-main.7cecb56b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/RoboFriends/precache-manifest.ba89d4a0b8b5c5f63e3c1d1fb65b9e36.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RoboFriends/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
